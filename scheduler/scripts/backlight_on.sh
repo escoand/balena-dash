4ecb93a89cf391ff8d474be933f26dcab266da4a
escoand <escoand@users.noreply.github.com>
2019-09-16 14:25:15 +0200

    add HDMI switching
    we need to build libcec on our own because upstream misses RPi support

--- scheduler/scripts/backlight_on.sh
+++ scheduler/scripts/backlight_on.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo -n 0 > /sys/class/backlight/rpi_backlight/bl_power
+
+output=$(/opt/vc/bin/tvservice -s)
+
+if [[ $output =~ "0x400000 [LCD]" ]]; then
+  echo -n 0 > /sys/class/backlight/rpi_backlight/bl_power
+else
+  echo as 0 | cec-client -s > /dev/null
+fi
